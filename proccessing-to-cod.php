38e440aaed4f382cb32b46651c6bbaf5672efcdf
Khairul Imran <105085586+khairulimran-97@users.noreply.github.com>
2024-03-04 06:46:43 +0800

    (HPOS) Change Order Status to "Processing-COD" if COD payment method is selected

--- proccessing-to-cod.php
+++ proccessing-to-cod.php
@@ -7,7 +7,6 @@ function custom_woocommerce_paid_order_status( $order_id ) {
 
     $order = wc_get_order( $order_id );
 
-    // Target your "your_payment_gateway_slug" with this conditional
     if ( is_object( $order ) && $order->get_payment_method() === 'cod' ) {
         $order->update_status( 'cod-processing' );
     }
